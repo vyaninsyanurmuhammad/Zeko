0505d6215a421222d1c05bc3d501c23a9c45a701
vyaninsyanurmuhammad <vyaninsya.nurmuhammad@gmail.com>
2024-03-20 20:08:17 +0700

    refactor (all): try to improve performance web at teams page

--- src/components/teams/teams-card-layout.tsx
+++ src/components/teams/teams-card-layout.tsx
@@ -33,7 +33,7 @@ const TeamsCardLayout = ({
           <span className="w-full text-white text-xl sm:text-2xl lg:text-4xl font-semibold truncate">
             {name}
           </span>
-          <span className="w-full flex justify-start lg:justify-center items-center text-start lg:text-center text-slate-400 group-hover:text-slate-100 text-sm sm:text-base lg:text-3xl font-medium">
+          <span className="w-full flex justify-start lg:justify-center items-center text-start lg:text-center text-slate-400 group-hover:text-slate-100 text-sm sm:text-base lg:text-3xl font-medium truncate">
             {position}
           </span>
         </div>
--- src/components/teams/teams-tagline.tsx
+++ src/components/teams/teams-tagline.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const TeamsTagline = () => {
   return (
     <>
-      <span className="text-3xl md:text-4xl lg:text-5xl font-medium text-white text-start w-full md:w-3/4 pt-24 pb-12 px-12 lg:px-24">
+      <span className="text-3xl md:text-4xl lg:text-5xl font-medium text-white text-start w-full h-fit md:w-3/4 pt-24 pb-12 px-12 lg:px-24">
         If you like to work in remote teams that value great work over loud
         voices, we’d love to hear from you!
       </span>
